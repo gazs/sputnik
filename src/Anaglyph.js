4dee4f03f0d5597177b777a8facbbd4c89e06aa4
Gáspár Körtesi <gaspar.kortesi@condenastint.com>
2019-07-12 10:44:19 +0100

    add anaglyph

---
+++ src/Anaglyph.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { getDownloadUrl } from './fortepan-api';
+
+
+export default ({fortepanObject, rotation, rightEyeX, rightEyeY}) => {
+  const imageSrc = getDownloadUrl(fortepanObject.filename);
+
+  return (
+<svg viewBox="0 0 500 500">
+  <defs>
+    <image
+      id="image"
+      xlinkHref={imageSrc}
+      width="200%"
+    />
+    <filter id="red">
+      <feColorMatrix type="matrix" values="1  0  0 0 0
+                                           0  0  0 0 0
+                                           0  0  0 0 0
+                                           0  0  0 0 1" />
+    </filter>
+    <filter id="cyan">
+      <feColorMatrix type="matrix" values="0  0  0 0 0
+                                           0  1  0 0 0
+                                           0  1  0 0 0
+                                           0  0  0 0 1" />
+    </filter>
+    <clipPath id="left">
+      <rect
+        width="100%"
+        height="100%"
+        x="0"
+        y="0"
+      />
+    </clipPath>
+    <clipPath id="right">
+      <rect
+        width="100%"
+        height="100%"
+        x="500"
+        y="0"
+      />
+    </clipPath>
+  </defs>
+  <use href="#image"
+    filter="url(#red)"
+    clipPath="url(#left)"
+    />
+  <use href="#image"
+    transform={`translate(${rightEyeX} ${rightEyeY})`}
+    style={{mixBlendMode: "screen"}}
+    filter="url(#cyan)"
+    clipPath="url(#right)"
+    />
+</svg>
+  );
+}
