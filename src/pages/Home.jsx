caa323c2fbbb1b78f34d462c3471b6a4039ec7d9
gazs <gazs@bergenocia.net>
2019-12-09 11:45:01 +0000

    shit pagination

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState} from 'react';
 import {Link} from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -9,8 +9,10 @@ import './Home.css';
 
 
 const Home = () => {
-  return <div className="grid">
-    {FortepanData.slice(480).map(x =>
+  const [page, setPage] = useState(0);
+
+  return <><div className="grid">
+    {FortepanData.slice(page*9, page*9+9).map(x =>
     <div key={x.filename}>
       <Link to={`${x.filename}`}>
         <img alt={x.title} src={`http://fortepan.hu/_photo/display/${x.filename}.jpg`} />
@@ -18,6 +20,9 @@ const Home = () => {
     </div>
     )}
     </div>
+      <button onClick={() => setPage(page-1)}>prev</button>
+      <button onClick={() => setPage(page+1)}>next</button>
+      </>
 }
 
 export default Home
